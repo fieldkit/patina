f1131b54ced349edf5724a3a9fda6d9698bd026b
Jacob Lewallen <jlewallen@gmail.com>
2023-05-22 18:10:59 -0700

    Localize unknown stations page.

--- lib/unknown_station_page.dart
+++ lib/unknown_station_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class NoSuchStationPage extends StatelessWidget {
   const NoSuchStationPage({super.key});
@@ -7,11 +8,11 @@ class NoSuchStationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Unknown Station"),
+        title: Text(AppLocalizations.of(context)!.unknownStationTitle),
       ),
       body: Center(
         child: ElevatedButton(
-          child: const Text('Back'),
+          child: Text(AppLocalizations.of(context)!.backButtonTitle),
           onPressed: () {
             Navigator.pop(context);
           },
