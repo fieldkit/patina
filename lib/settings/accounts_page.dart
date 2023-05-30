00e9e59bcf0dfd16fb82371d126338bc7c68507c
Jacob Lewallen <jlewallen@gmail.com>
2023-05-30 11:14:52 -0700

    Move splash to its own file.

--- lib/settings/accounts_page.dart
+++ lib/settings/accounts_page.dart
@@ -5,6 +5,7 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:provider/provider.dart';
 
 import '../app_state.dart';
+import '../splash_screen.dart';
 
 class AccountsPage extends StatelessWidget {
   const AccountsPage({super.key});
@@ -217,21 +218,5 @@ class ProvideAccountsWidget extends StatelessWidget {
             return const FullScreenLogo();
           }
         });
-  }
-}
-
-const Color logoBlue = Color.fromRGBO(27, 128, 201, 1);
-
-class FullScreenLogo extends StatelessWidget {
-  const FullScreenLogo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
-          Image.asset("resources/images/logo_fk_blue.png"),
-          const Padding(padding: EdgeInsets.only(top: 50), child: SizedBox(width: 100, height: 100, child: CircularProgressIndicator()))
-        ]));
   }
 }
