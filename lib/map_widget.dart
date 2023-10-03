cbf8e9048b02710c6474db19d7f23ee5249ff0d7
Kate Kuehl <katekuehl@gmail.com>
2023-10-03 14:21:35 -0500

    Add feature to  to location if permission if granted

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,25 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
   const Map({super.key});
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  LatLng? _userLocation;
+  MapController mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  Future<void> _getUserLocation() async {
+    final location = Location();
+    var hasPermission = await location.hasPermission();
+
+    if (hasPermission == PermissionStatus.denied) {
+      hasPermission = await location.requestPermission();
+    }
+
+    if (hasPermission == PermissionStatus.granted) {
+      final currentLocation = await location.getLocation();
+      setState(() {
+        _userLocation =
+            LatLng(currentLocation.latitude!, currentLocation.longitude!);
+      });
+
+      mapController.move(_userLocation!, 12); // Zoom level 12 as in your code
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Location permission denied!")),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
-        center: LatLng(48.864716, 2.349014),
-        zoom: 9.2,
+        center: _userLocation ??
+            LatLng(34.0312492,
+                -118.269107), // default to Conservify if location not yet retrieved
+        zoom: 12,
       ),
-      nonRotatedChildren: const [
-        /* Can we style this differently? Maybe if not we can hide over time?
-        AttributionWidget.defaultWidget(
-          source: 'OpenStreetMap contributors',
-          onSourceTapped: null,
-        ),
-        */
-      ],
+      nonRotatedChildren: const [],
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import flutter_secure_storage_macos
 import geolocator_apple
+import location
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
