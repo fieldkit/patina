727401dd16b92abd0ba0e0184948c436ad7e46a2
Kate Kuehl <katekuehl@gmail.com>
2023-10-03 22:51:02 -0500

    Add beginning toggle code and icon

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // <- Import this for SystemChrome
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
@@ -13,11 +14,24 @@ class Map extends StatefulWidget {
 class _MapState extends State<Map> {
   LatLng? _userLocation;
   MapController mapController = MapController();
+  bool _isFullscreen = false; // <- Add this
 
   @override
   void initState() {
     super.initState();
     _getUserLocation();
+  }
+
+  // Toggle fullscreen
+  void _toggleFullscreen() {
+    if (_isFullscreen) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    } else {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    }
+    setState(() {
+      _isFullscreen = !_isFullscreen;
+    });
   }
 
   Future<void> _getUserLocation() async {
@@ -35,7 +49,7 @@ class _MapState extends State<Map> {
             LatLng(currentLocation.latitude!, currentLocation.longitude!);
       });
 
-      mapController.move(_userLocation!, 12); // Zoom level 12 as in your code
+      mapController.move(_userLocation!, 12);
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Location permission denied!")),
@@ -45,20 +59,37 @@ class _MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      mapController: mapController,
-      options: MapOptions(
-        center: _userLocation ??
-            LatLng(34.0312492,
-                -118.269107), // default to Conservify if location not yet retrieved
-        zoom: 12,
-      ),
-      nonRotatedChildren: const [],
+    return Stack(
       children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'org.fieldkit.app',
+        FlutterMap(
+          mapController: mapController,
+          options: MapOptions(
+            center: _userLocation ?? LatLng(34.0312492, -118.269107),
+            zoom: 12,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'org.fieldkit.app',
+            ),
+          ],
         ),
+        Positioned(
+          bottom: 12.0,
+          right: 12.0,
+          child: IconButton(
+            iconSize: 36.0,
+            icon: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                _isFullscreen ? Icons.fullscreen_exit : Icons.fullscreen,
+                color: Colors.grey,
+                size: 28.0,
+              ),
+            ),
+            onPressed: _toggleFullscreen,
+          ),
+        )
       ],
     );
   }
