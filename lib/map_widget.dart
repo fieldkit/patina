bec0636752c1184231e24f96ceab3b23795c842f
Kate Kuehl <katekuehl@gmail.com>
2023-10-05 11:38:47 -0500

    Add starter to self location

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,7 +1,7 @@
 import 'package:fk/fullscreen_map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+// import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
