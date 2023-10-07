f1aba9301ce0066ebe5a6b86e2f26adafbdc3746
Kate Kuehl <katekuehl@gmail.com>
2023-10-06 19:13:40 -0500

    Add account tests

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -2,8 +2,6 @@ import 'package:fk/settings/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:fk/settings/accounts_page.dart';
-import 'package:fk/reader/screens.dart';
 
 void main() {
   group('SettingsTab', () {
