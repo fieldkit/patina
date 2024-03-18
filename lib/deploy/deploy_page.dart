6ecf87f191ac406601743f01155ab37beb3e9589
Jacob Lewallen <jlewallen@gmail.com>
2024-03-18 14:41:42 -0700

    Basic deployment form.

--- lib/deploy/deploy_page.dart
+++ lib/deploy/deploy_page.dart
@@ -1,10 +1,15 @@
 import 'package:fk/app_state.dart';
+import 'package:fk/common_widgets.dart';
+import 'package:fk/deploy/configure_schedule_page.dart';
+import 'package:fk/diagnostics.dart';
 import 'package:fk/gen/bridge_definitions.dart';
 import 'package:fk/map_widget.dart';
 import 'package:fk/providers.dart';
 import 'package:fk/unknown_station_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:provider/provider.dart';
 
 class DeployStationPage extends StatelessWidget {
@@ -16,7 +21,8 @@ class DeployStationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Widget map = const SizedBox(height: 200, child: MapWidget());
+    const Widget map = SizedBox(height: 200, child: MapWidget());
+
     return Scaffold(
         appBar: AppBar(
           title: Text(AppLocalizations.of(context)!.deployTitle),
@@ -34,7 +40,56 @@ class DeployStationPage extends StatelessWidget {
           ),
         ),
         body: ListView(
-          children: [map],
+          children: [map, DeployFormWidget()],
+        ));
+  }
+}
+
+class DeployFormWidget extends StatefulWidget {
+  const DeployFormWidget({super.key});
+
+  @override
+  State<DeployFormWidget> createState() => _DeployFormState();
+}
+
+class _DeployFormState extends State<DeployFormWidget> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+
+    return FormBuilder(
+        key: _formKey,
+        child: Column(
+          children: [
+            FormBuilderTextField(
+              name: 'location',
+              keyboardType: TextInputType.text,
+              decoration:
+                  InputDecoration(labelText: localizations.deployLocation),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+              ]),
+            ),
+            const ScheduleWidget(),
+            ElevatedTextButton(
+              onPressed: () async {
+                if (_formKey.currentState!.saveAndValidate()) {
+                  final values = _formKey.currentState!.value;
+                  Loggers.ui.i("$values");
+                  final String location = values["location"];
+                  final int scheduleEvery = int.parse(values["scheduleEvery"]);
+                  final UnitOfTime scheduleUnit = values["scheduleUnit"];
+                  Loggers.ui.i("$location $scheduleEvery $scheduleUnit");
+                }
+              },
+              text: localizations.deployButton,
+            ),
+          ]
+              .map((child) =>
+                  Padding(padding: const EdgeInsets.all(8), child: child))
+              .toList(),
         ));
   }
 }
