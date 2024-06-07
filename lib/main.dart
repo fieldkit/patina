21cf44aedab7958843575a7e9480f53398529ee2
Kate Kuehl <katekuehl@Kates-MacBook-Air.local>
2024-06-07 16:30:27 -0500

    Fix the bug where the screen can change to landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fk/loading_widget.dart';
+import 'package:flutter/services.dart';
 
 import 'gen/frb_generated.dart';
 
@@ -11,5 +12,11 @@ void main() async {
   // is only available once that binding has been initialized.'
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const LoadingWidget());
+  // Set preferred orientations to portraitUp and portraitDown
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const LoadingWidget());
+  });
 }
