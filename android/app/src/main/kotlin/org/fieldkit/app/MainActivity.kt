755f7fd4b0ef95c1e3950bc678b08331cf17ae1d
Jacob Lewallen <jlewallen@gmail.com>
2024-06-28 14:35:52 -0700

    Workaround crash on back gesture bug.
    https://github.com/flutter/flutter/issues/117061
    https://medium.com/stuart-engineering/keep-flutter-running-background-on-android-6ffc85be0234

--- android/app/src/main/kotlin/org/fieldkit/app/MainActivity.kt
+++ android/app/src/main/kotlin/org/fieldkit/app/MainActivity.kt
@@ -1,6 +1,23 @@
 package org.fieldkit.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.os.Bundle
+import android.content.Intent
+import android.content.Context
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "org.fieldkit/app_retain"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, res ->
+            if (call.method == "sendToBackground") {
+                moveTaskToBack(true)
+                res.success(null)
+            }
+        }
+    }
 }
