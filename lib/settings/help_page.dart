d9b155a59dc79015f6dc3894bcda411589de4b02
Kate Kuehl <katekuehl@gmail.com>
2024-07-02 18:35:17 +0000

    Revert "Fix Help to Settings back navigation and app versioning number"
    This reverts commit 67cc0f3d641a88823478062b020fd2c9cadf9a6c

--- lib/settings/help_page.dart
+++ lib/settings/help_page.dart
@@ -1,5 +1,5 @@
 import 'package:fk/diagnostics.dart';
-import 'package:fk/reader/screens.dart';
+import 'package:fk/settings/settings_page.dart';
 import 'package:flows/flows.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -7,39 +7,52 @@ import 'package:loader_overlay/loader_overlay.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/link.dart';
 
-import '../home_page.dart';
+import '../reader/screens.dart';
 
-class HelpPage extends StatelessWidget {
+class HelpPage extends StatefulWidget {
   const HelpPage({super.key});
 
-  Future<String> _getAppVersion() async {
-    final PackageInfo info = await PackageInfo.fromPlatform();
-    return info.version;
+  @override
+  State<HelpPage> createState() => _HelpPageState();
+}
+
+class _HelpPageState extends State<HelpPage> {
+  bool isArrowUp = false;
+  String appVersion = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
   }
 
-  final _textStyle = const TextStyle(
-      fontSize: 14.0,
-      fontFamily: 'Avenir',
-      fontWeight: FontWeight.normal,
-      color: Colors.black,
-      decoration: TextDecoration.none);
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      appVersion = info.version;
+    });
+  }
+
+  final _textStyle = const TextStyle(fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(localizations.helpTitle),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context);
-            final homePageState =
-                context.findAncestorStateOfType<HomePageState>();
-            if (homePageState != null) {
-              homePageState.setPageIndex(2);
-            }
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) => const SettingsTab(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+              ),
+            );
           },
         ),
       ),
@@ -53,8 +66,7 @@ class HelpPage extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => QuickFlow(
-                        start: const StartFlow(prefix: "onboarding"),
-                        onForwardEnd: () => onDone(context))),
+                        start: const StartFlow(prefix: "onboarding"), onForwardEnd: () => onDone())),
               );
             },
           ),
@@ -91,72 +103,66 @@ class HelpPage extends StatelessWidget {
           const Divider(),
           ListTile(
             title: Text(localizations.appVersion),
-          ),
-          FutureBuilder<String>(
-            future: _getAppVersion(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text(
-                  localizations.errorLoadingVersion,
-                  style: _textStyle,
-                );
-              } else {
-                return Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: 30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          style: DefaultTextStyle.of(context).style,
-                          children: [
-                            TextSpan(
-                              text: localizations.appVersion,
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: ': ',
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: snapshot.data ?? '',
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: '\n',
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: getCommitRefName() ?? localizations.developerBuild,
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: '\n',
-                              style: _textStyle,
-                            ),
-                            TextSpan(
-                              text: getCommitSha() ?? localizations.developerBuild,
-                              style: _textStyle,
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }
+            trailing: Icon(
+              isArrowUp ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+              size: 30.0,
+            ),
+            onTap: () {
+              setState(() {
+                isArrowUp = !isArrowUp;
+              });
             },
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Column(
+              children: [
+                if (isArrowUp)
+                  RichText(
+                    text: TextSpan(
+                      style: DefaultTextStyle.of(context).style,
+                      children: [
+                        TextSpan(
+                          text: localizations.appVersion,
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: ': ', // Add a colon and space
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: appVersion,
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: '\n',
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: getCommitRefName() ??
+                              localizations.developerBuild,
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: '\n',
+                          style: _textStyle,
+                        ),
+                        TextSpan(
+                          text: getCommitSha() ?? localizations.developerBuild,
+                          style: _textStyle,
+                        ),
+                      ],
+                    ),
+                  )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
-
-  void onDone(BuildContext context) {
-    Navigator.pop(
-        context); // Return to the help page after the tutorial guide is completed
+  
+  onDone() {
+    return Navigator.pop(context); // Return to the help page after the tutorial guide is completed
   }
 }
